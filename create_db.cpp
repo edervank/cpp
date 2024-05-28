fe65c76323a8d3c55d933a76e0f047c158ecda12
Edervan Kunz <edervan.kunz2013@gmail.com>
2024-05-28 10:45:18 -0300

    wip

--- create_db.cpp
+++ create_db.cpp
@@ -1,2 +1,11 @@
+#include <sqlite3.h>
+#include <iostream>
+using namespace std;
 
+int main(int argc, char* argv[]){
+     sqlite3 *db;
+   int rc;
 
+   rc = sqlite3_open("test.db", &db);
+    return 0;
+}
--- weather_cpp.cpp
+++ weather_cpp.cpp
@@ -11,6 +11,9 @@ int main(int argc, char* argv[]) {
 
     cout << "Latitude: " << latitude << endl;
     cout << "Longitude: " << longitude << endl;
+    
 
+
+    
     return 0;
 }
